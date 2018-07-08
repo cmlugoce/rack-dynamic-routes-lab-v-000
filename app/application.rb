8e15421c6623102a060fe811db29bd35c29c7f39
Cristina Lugo <cmlugoce@syr.edu>
2018-07-08 20:19:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,12 @@ class Application
   
   @@items = []
   
-  
+   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ if req.patch.match(/items/)
+   item_name = req.path.split("/items/").last 
+  if item = @@items.find
   
   
   
